fe2e15c660c6798affc387c3bbc60c44fe536789
STS <STS@192.168.56.1>
2025-12-31 20:52:57 +0100

    final

--- demogQL2/src/main/java/rs/spai/demogQL2/HomeController.java
+++ demogQL2/src/main/java/rs/spai/demogQL2/HomeController.java
@@ -1,4 +1,5 @@
 package rs.spai.demogQL2;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -6,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String redirectToGraphiQL() {
         return "redirect:/graphiql";
     }
 }
